df8cb31bb004ddc602654cd1354c5e1c153fe776
Karoife <joserodolfo_coco@hotmail.com>
2023-05-21 11:41:33 -0600

    First version of the project

--- src/detect.py
+++ src/detect.py
@@ -37,6 +37,9 @@ class AudioStream(object):
         self.fig, (timeG, freqG) = plt.subplots(2, figsize=(15,7))
         self.fig.canvas.mpl_connect('button_press_event', self.onClick)
 
+        self.line, = timeG.plot(x, np.random.rand(self.chunk), "-", lw=2)
+        self.line_fft, = freqG.plot(y, np.random.rand(self.chunk), "-", lw=2)
+
         # limite de la grafica tiempo
         timeG.set_title("Dominio del Tiempo")
         timeG.set_ylabel=("Amplitud")
@@ -102,4 +105,65 @@ class AudioStream(object):
 
             # Detecciomn de nota musical 
             if 980 <= f_vec[max_loc] <= 990:
-                
+                print("B5 Si")
+            if 865 <= f_vec[max_loc] <= 895:
+                print("A5 La")
+            if 775 <= f_vec[max_loc] <= 800:
+                print("G5 Sol")
+            if 690 <= f_vec[max_loc] <= 710:
+                print("F5 Fa")
+            if 650 <= f_vec[max_loc] <= 670:
+                print("E5 Mi")
+            if 585 <= f_vec[max_loc] <= 595:
+                print("D5 Re")
+            if 515 <= f_vec[max_loc] <= 550:
+                print("C5 DO")
+            if 490 <= f_vec[max_loc] <= 500:
+                print("B4 Si")
+            if 437 <= f_vec[max_loc] <= 447:
+                print("A4 La")
+            if 390 <= f_vec[max_loc] <= 400:
+                print("G4 Sol")
+            if 345 <= f_vec[max_loc] <= 355:
+                print("F4 Fa")
+            if 325 <= f_vec[max_loc] <= 335:
+                print("E4 Mi")
+            if 290 <= f_vec[max_loc] <= 300:
+                print("D4 Re")
+            if 255 <= f_vec[max_loc] <= 280:
+                print("C4 Do")
+            if 243 <= f_vec[max_loc] <= 253:
+                print("B3 Si")
+            if 215 <= f_vec[max_loc] <= 225:
+                print("A3 La")
+            if 192 <= f_vec[max_loc] <= 202:
+                print("G3 Sol")
+            if 172 <= f_vec[max_loc] <= 177:
+                print("F3 Fa")
+            if 162 <= f_vec[max_loc] <= 167:
+                print("E3 Mi")
+            if 144 <= f_vec[max_loc] <= 150:
+                print("D3 Re")
+            if 127 <= f_vec[max_loc] <= 133:
+                print("C3 Do")
+
+            self.fig.canvas.draw()
+            self.fig.canvas.flush_events()
+            frame_count += 1
+
+        else:
+            self.fr = frame_count / (time.time() - start_time)
+            print("average frame rate = {:0f} FPS".format(self.fr))
+            self.exit_app()
+
+
+    def exit_app(self):
+        print("cierra")
+        self.p.close(self.stream)
+        
+    def onClick(self, event):
+        self.pause = True
+
+
+if __name__ == "__main__":
+    AudioStream()
