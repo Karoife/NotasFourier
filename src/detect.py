2bf173b52792332468ebdd4978db19189e5c4d3b
Karoife <joserodolfo_coco@hotmail.com>
2023-05-21 07:28:27 -0600

    Freq selectrion

--- src/detect.py
+++ src/detect.py
@@ -84,4 +84,22 @@ class AudioStream(object):
             self.line.set_ydata(data_np)
 
             # Calculo de la transformada rapida de fourier
-            
+            yf = np.fft.fft(data_int)
+
+            # pasar los valores en frecuencia a la grafica
+            self.line_fft.set_ydata(
+                np.abs(yf[0:self.chunk])/ (128 * self.chunk)
+            )
+
+            # identificar el pico de frecuncia en el vector
+            f_vec = self.rate*np.arange(self.chunk/2)/self.chunk
+            mic_low_freq = 40
+            low_freq_loc = np.argmin(np.abs(f_vec-mic_low_freq))
+            fft_data = (np.abs(np.fft.fft(data_int))[0:int(np.floor(self.chunk/2))])/self.chunk
+
+            # pico mas alto en frecuencia
+            max_loc = np.argmax(fft_data[low_freq_loc:])+low_freq_loc
+
+            # Detecciomn de nota musical 
+            if 980 <= f_vec[max_loc] <= 990:
+                
